0a82999ba408962c3c2db799d8da1b2cacc1c182
Lukasz Nidecki <lnidecki@gmail.com>
2025-08-05 12:34:24 +0200

    less resources for argo

--- main.tf
+++ main.tf
@@ -83,6 +83,89 @@ resource "helm_release" "argocd" {
   namespace  = "argocd"
 
   create_namespace = true
+
+  set {
+    name  = "notifications.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "dex.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "applicationSet.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "controller.resources.limits.cpu"
+    value = "200m"
+  }
+  set {
+    name  = "controller.resources.limits.memory"
+    value = "256Mi"
+  }
+  set {
+    name  = "controller.resources.requests.cpu"
+    value = "100m"
+  }
+  set {
+    name  = "controller.resources.requests.memory"
+    value = "128Mi"
+  }
+
+  set {
+    name  = "server.resources.limits.cpu"
+    value = "200m"
+  }
+  set {
+    name  = "server.resources.limits.memory"
+    value = "256Mi"
+  }
+  set {
+    name  = "server.resources.requests.cpu"
+    value = "100m"
+  }
+  set {
+    name  = "server.resources.requests.memory"
+    value = "128Mi"
+  }
+
+  set {
+    name  = "repoServer.resources.limits.cpu"
+    value = "300m"
+  }
+  set {
+    name  = "repoServer.resources.limits.memory"
+    value = "512Mi"
+  }
+  set {
+    name  = "repoServer.resources.requests.cpu"
+    value = "150m"
+  }
+  set {
+    name  = "repoServer.resources.requests.memory"
+    value = "256Mi"
+  }
+
+  set {
+    name  = "redis.resources.limits.cpu"
+    value = "100m"
+  }
+  set {
+    name  = "redis.resources.limits.memory"
+    value = "256Mi"
+  }
+  set {
+    name  = "redis.resources.requests.cpu"
+    value = "50m"
+  }
+  set {
+    name  = "redis.resources.requests.memory"
+    value = "128Mi"
+  }
 }
 
 
