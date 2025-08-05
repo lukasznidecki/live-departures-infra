63dc581f79a1b17a2c534c225da3e243b1731184
Łukasz <lnidecki@gmail.com>
2025-08-05 12:40:55 +0200

    Add Argo CD Helm deployment (#2)
    * Add Argo CD Helm deployment
    
    * less resources for argo

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 }
 
@@ -50,6 +58,114 @@ resource "google_container_cluster" "autopilot" {
   }
 
   deletion_protection = false
+}
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.autopilot.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.autopilot.master_auth[0].cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.autopilot.endpoint
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.autopilot.master_auth[0].cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
+
+  create_namespace = true
+
+  set {
+    name  = "notifications.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "dex.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "applicationSet.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "controller.resources.limits.cpu"
+    value = "200m"
+  }
+  set {
+    name  = "controller.resources.limits.memory"
+    value = "256Mi"
+  }
+  set {
+    name  = "controller.resources.requests.cpu"
+    value = "100m"
+  }
+  set {
+    name  = "controller.resources.requests.memory"
+    value = "128Mi"
+  }
+
+  set {
+    name  = "server.resources.limits.cpu"
+    value = "200m"
+  }
+  set {
+    name  = "server.resources.limits.memory"
+    value = "256Mi"
+  }
+  set {
+    name  = "server.resources.requests.cpu"
+    value = "100m"
+  }
+  set {
+    name  = "server.resources.requests.memory"
+    value = "128Mi"
+  }
+
+  set {
+    name  = "repoServer.resources.limits.cpu"
+    value = "300m"
+  }
+  set {
+    name  = "repoServer.resources.limits.memory"
+    value = "512Mi"
+  }
+  set {
+    name  = "repoServer.resources.requests.cpu"
+    value = "150m"
+  }
+  set {
+    name  = "repoServer.resources.requests.memory"
+    value = "256Mi"
+  }
+
+  set {
+    name  = "redis.resources.limits.cpu"
+    value = "100m"
+  }
+  set {
+    name  = "redis.resources.limits.memory"
+    value = "256Mi"
+  }
+  set {
+    name  = "redis.resources.requests.cpu"
+    value = "50m"
+  }
+  set {
+    name  = "redis.resources.requests.memory"
+    value = "128Mi"
+  }
 }
 
 
