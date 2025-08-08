004a6716ff97d104af867f39c37218058cd7a00c
Lukasz Nidecki <lnidecki@gmail.com>
2025-08-08 16:43:58 +0200

    fix formatting

--- main.tf
+++ main.tf
@@ -63,15 +63,15 @@ resource "google_container_cluster" "autopilot" {
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
-  host  = google_container_cluster.autopilot.endpoint
-  token = data.google_client_config.default.access_token
+  host                   = google_container_cluster.autopilot.endpoint
+  token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.autopilot.master_auth[0].cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
-    host  = google_container_cluster.autopilot.endpoint
-    token = data.google_client_config.default.access_token
+    host                   = google_container_cluster.autopilot.endpoint
+    token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(google_container_cluster.autopilot.master_auth[0].cluster_ca_certificate)
   }
 }
