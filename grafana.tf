1cc2214cefbf2032cc0ae002f1e38f3f9d2df750
Lukasz Nidecki <lnidecki@gmail.com>
2025-08-12 19:24:02 +0200

    install prometheus CRD

--- grafana.tf
+++ grafana.tf
@@ -18,7 +18,7 @@ resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
   version    = "25.8.0"
-  namespace  = kubernetes_namespace.monitoring.metadata[0].named
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
 
   set {
     name  = "installCRDs"
